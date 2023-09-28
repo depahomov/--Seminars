8c86d95e38caef20de786e495a5eca9f551077cb
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-09-28 22:55:44 +0700

    init git, add Sem1 and Sem2

---
+++ Sem1/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
+*/
+
+/*
+ System.Console.WriteLine("Input number");
+ int num = Convert.ToInt32(Console.ReadLine());
+ System.Console.WriteLine($"Your number -> {num} ");
+*/
+
+/*
+//равенство квадрату числа
+System.Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int quad = num*num
+System.Console.WriteLine($"q number -> {num*num} ");
+System.Console.WriteLine("Input 3 digit number");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99 && num < 1000)
+{   System.Console.WriteLine($"last number -> {num % 10} "); 
+}
+else
+{
+   System.Console.WriteLine("Not tree-digit number");
+}
+*/
+
+/*
+5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"
+
+17:36
+1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+a = 25; b = 5 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> да
+a = -3 b = 9 -> нет
+*/
+
+/*
+System.Console.WriteLine("Input number:");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -n;
+while (count <=n ) 
+     System.Console.Write(count++ + " ");
+System.Console.WriteLine("Input number:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input number:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b)
+{
+    System.Console.WriteLine($"{a} is quad {b}");
+}
+else
+{
+    System.Console.WriteLine($"{a} is NOT quad {b}");
+}
+*/
