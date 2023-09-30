8eed36c3212b10068bb5f28cabb1d6192465cac5
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-09-30 23:20:44 +0700

    решена задача 10

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Урок 2. Базовые алгоритмы
+
+/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+System.Console.WriteLine("Вывод второй цифры из трёхзначного числа");
+int ShowSecondDigit(int number)
+{
+    number = number / 10;
+    number = number % 10;
+    return number;
+}
+
+System.Console.Write("Введите трёхзначное число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"{num} -> {ShowSecondDigit(num)}");
