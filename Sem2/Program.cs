c877488ac21d5684eece70f4ba22f6a86b53b1b6
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-09-30 19:46:59 +0700

    проверка делимости без остатка

--- Sem2/Program.cs
+++ Sem2/Program.cs
@@ -7,3 +7,21 @@
 16, 4  -> кратно
 */
 
+void CheckMultiplicity(int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        System.Console.WriteLine($"{num1}, {num2} -> кратно");
+    }
+    else
+    {
+        System.Console.WriteLine($"{num1}, {num2} -> не кратно, остаток {num1 % num2}");        
+    }
+}
+
+System.Console.Write("Input first number:  ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input second number:  ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+CheckMultiplicity(n1, n2);
