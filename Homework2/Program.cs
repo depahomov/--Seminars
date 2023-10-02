95089e1f8f006b60f493d9e0f3df72fbd6c6717e
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-10-02 17:22:22 +0700

    решил задачу 15

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -27,6 +27,8 @@ else
 {
     System.Console.WriteLine("Введённое число не трёхзначное!");
 }
+System.Console.WriteLine();
+
 
 /*
 Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
@@ -58,4 +60,46 @@ else
 {
     System.Console.WriteLine("Третьей цифры нет!");
 }
+System.Console.WriteLine();
 
+
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+System.Console.WriteLine("Проверка, является ли день выходным по его номеру");
+
+bool CheckNumber(int num2) //проверка, является ли введённое число корректным (т.е. от 1 до 7){
+ {   
+    if (num2 > 0 & num2 < 8)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+System.Console.Write("Введите целое число от 1 до 7, обозначающее день недели:  ");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+
+if (numberDay > 5 && CheckNumber(numberDay) == true)
+{
+    System.Console.WriteLine($"{numberDay} -> да");
+}
+else
+{
+    if (numberDay < 6  && CheckNumber(numberDay) == true)
+    {
+        System.Console.WriteLine($"{numberDay} -> нет");
+    }
+    else
+    {
+        System.Console.WriteLine($"{numberDay} -> не является днём недели");
+    }
+
+}
