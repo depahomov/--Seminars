9d5d3a6db09bef3eec1a3f16e447b12f51c23285
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-10-02 16:54:41 +0700

    решил задачу 13 и доработал задачу 10

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -6,6 +6,8 @@
 782 -> 8
 918 -> 1
 */
+
+
 System.Console.WriteLine("Вывод второй цифры из трёхзначного числа");
 int ShowSecondDigit(int number)
 {
@@ -16,5 +18,44 @@ int ShowSecondDigit(int number)
 
 System.Console.Write("Введите трёхзначное число:  ");
 int num = Convert.ToInt32(Console.ReadLine());
+int numAbs = Math.Abs(num);
+if (numAbs > 99 & numAbs < 1000)
+{
+   System.Console.WriteLine($"{num} -> {ShowSecondDigit(numAbs)}");
+}
+else
+{
+    System.Console.WriteLine("Введённое число не трёхзначное!");
+}
 
-System.Console.WriteLine($"{num} -> {ShowSecondDigit(num)}");
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+System.Console.WriteLine("Вывод третьей цифры заданного числа (при её наличии)");
+
+int ShowThirdDigit(int number1)
+{
+    while (number1 > 999)
+    {
+        number1 = number1 / 10;
+    }
+    number1 = number1 % 10;
+    return number1;
+}
+
+System.Console.Write("Введите целое число:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num1Abs = Math.Abs(num1);
+if (num1Abs > 99)
+{
+    System.Console.WriteLine($"{num1} -> {ShowThirdDigit(num1Abs)}");
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет!");
+}
+
