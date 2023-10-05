0d32bb9056fbcec385e29d1712e3fceea11d61e1
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-10-05 17:23:32 +0700

    решение задачи 1 семинар 4

---
+++ Sem4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите програму, которая принимает на вход число и выдаёт количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+/*
+int DigitCount(int num)
+{
+    num = Math.Abs(num);
+    int count = 1;
+    while(num > 10)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+System.Console.Write("Input num:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(DigitCount(num));
+*/
+
+// Напишите программу
