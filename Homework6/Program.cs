01fbee8ab82badafbb88a91bc03d36d8345018d6
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-10-09 22:25:19 +0700

    finised homework 6

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -47,3 +47,25 @@ System.Console.WriteLine($" -> {SumOfPositiv(myArray)}");
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+void PointIntersection(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2 && b1 == b2) System.Console.WriteLine("Прямые совпадают");
+    if (k1 == k2 && b1 != b2) System.Console.WriteLine("Прямые параллельны");
+    if (k1 != k2 && b1 != b2)
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        System.Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x:f1}; {y:f1})");
+    }
+}
+System.Console.Write("Input b1: ");
+double myb1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input k1: ");
+double myk1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Input b2: ");
+double myb2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input k2: ");
+double myk2 = Convert.ToInt32(Console.ReadLine());
+
+PointIntersection(myk1, myb1, myk2, myb2);
