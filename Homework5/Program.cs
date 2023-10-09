37e4b66f7fad503a41819cf9b6cc9475565ddb6f
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-10-09 16:42:23 +0700

    переимонование папки в Homework6

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -2,6 +2,7 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+/*
 void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -38,3 +39,11 @@ int m = Convert.ToInt32(Console.ReadLine());
 int[] myArray = InputArray(m);
 PrintArray(myArray);
 System.Console.WriteLine($" -> {SumOfPositiv(myArray)}");
+*/
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
