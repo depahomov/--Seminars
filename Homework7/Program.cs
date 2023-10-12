19d8a0add62f30f12e823276a47fbc60ca08ac93
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-10-12 23:28:44 +0700

    начало решения задачи 2 из д.з.7

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -19,6 +19,7 @@ m = 3, n = 4, minLimitRandom = -10, maxLimitRandom = 10
 8    7,8    -7,1    9
  */
 
+/*
 int InputInteger(string message)
 {
     Console.Write(message);
@@ -52,13 +53,98 @@ double[,] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRando
         }
 }
 
-
-
-
 int rows = InputInteger("Введите количество строк: ");
 int columns = InputInteger("Введите количество стоблцов: ");
 int min = InputInteger("Введите минимальное значение диапазона: ");
 int max = InputInteger("Введите максимальное значение диапазона: ");
 
 double[,] myArray = CreateRandomMatrix(rows, columns, min, max);
+PrintArray(myArray);
+*/
+/*
+Задача 2.
+Внутри класса Answer напишите методы CreateIncreasingMatrix, PrintArray, 
+FindNumberByPosition и PrintCheckIfError.
+
+Метод CreateIncreasingMatrix должен создавать матрицу заданной размерности, 
+с каждым новым элементом увеличивающимся на определенное число. 
+Метод принимает на вход три числа 
+(n - количество строк матрицы, m - количество столбцов матрицы, 
+k - число, на которое увеличивается каждый новый элемент) 
+и возвращает матрицу, удовлетворяющую этим условиям.
+
+Метод PrintArray должен выводить на экран сгенерированную методом 
+CreateIncreasingMatrix матрицу. 
+Элементы матрицы должны быть выведены через символ табуляции для более читаемого вывода.
+
+Метод FindNumberByPosition принимает на вход сгенерированную матрицу 
+и числа x и y - позиции элемента в матрице. Если указанные координаты 
+находятся за пределами границ массива, метод должен вернуть массив с нулевым значением. 
+Если координаты находятся в пределах границ, метод должен вернуть массив с двумя значениями: 
+значением числа в указанной позиции, а второй элемент должен быть равен 0, чтобы показать, 
+что операция прошла успешно без ошибок.
+
+Метод PrintCheckIfError должен принимать результат метода FindNumberByPosition 
+и числа x и y - позиции элемента в матрице. Метод должен проверить, 
+был ли найден элемент в матрице по указанным координатам (x и y), 
+используя результаты из метода FindNumberByPosition. Если такого элемента нет, 
+вывести на экран "There is no such index". 
+Если элемент есть, вывести на экран "The number in [{x}, {y}] is {значение}".
+*/
+int InputInteger(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] matrix)
+{
+    // Введите свое решение ниже
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int[,] CreateIncreasingMatrix(int n, int m, int k)
+{
+    // Введите свое решение ниже
+    int[,] array = new int[n, m];
+    int temp = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            array[i, j] = temp;
+            temp += k;
+        }
+    }
+    return array;
+}
+
+/*
+int[] FindNumberByPosition(int[,] matrix, int rowPosition, int columnPosition)
+{
+    // Введите свое решение ниже
+
+
+}
+
+public static void PrintCheckIfError(int[] results, int X, int Y)
+{
+    // Введите свое решение ниже
+
+
+}
+*/
+int rows = InputInteger("Введите количество строк: ");
+int columns = InputInteger("Введите количество стоблцов: ");
+int number = InputInteger("Введите число, на которое увеличивается каждый новый элемент: ");
+
+int[,] myArray = CreateIncreasingMatrix(rows, columns, number);
 PrintArray(myArray);
