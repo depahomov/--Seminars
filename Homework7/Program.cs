bec7f9a14d69fe740980825b0abca5c68321f98c
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-10-16 18:25:22 +0700

    решение задачи 3 из д.з.7

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -1,4 +1,5 @@
-﻿/* Д/З 7. Задача 1. 
+﻿/* Д.З 7.
+--- Задача 1. -------------------------------------------------------
 Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
 Внутри класса Answer напишите метод CreateRandomMatrix, который принимал бы числа m и n 
@@ -61,8 +62,10 @@ int max = InputInteger("Введите максимальное значение
 double[,] myArray = CreateRandomMatrix(rows, columns, min, max);
 PrintArray(myArray);
 */
+
 /*
-Задача 2.
+------------- Задача 2. ----------------------------------------------
+
 Внутри класса Answer напишите методы CreateIncreasingMatrix, PrintArray, 
 FindNumberByPosition и PrintCheckIfError.
 
@@ -91,6 +94,8 @@ CreateIncreasingMatrix матрицу.
 вывести на экран "There is no such index". 
 Если элемент есть, вывести на экран "The number in [{x}, {y}] is {значение}".
 */
+
+/*
 int InputInteger(string message)
 {
     Console.Write(message);
@@ -138,7 +143,7 @@ int[] FindNumberByPosition(int[,] matrix, int rowPosition, int columnPosition)
     }
     else
     {
-        int arrayElement = matrix[rowPosition - 1, columnPosition - 1];
+        int arrayElement = matrix[rowPosition, columnPosition];
         int[] arrayIn = { arrayElement, 0 };
         return arrayIn;
     }
@@ -160,8 +165,147 @@ void PrintCheckIfError(int[] results, int X, int Y)
 
 int rows = InputInteger("Введите количество строк: ");
 int columns = InputInteger("Введите количество стоблцов: ");
-int number = InputInteger("Введите число, на которое увеличивается каждый новый элемент: ");
+int number = InputInteger("Введите число, на которое увеличивается каждый новый 
+элемент: ");
 
 int[,] myArray = CreateIncreasingMatrix(rows, columns, number);
 PrintArray(myArray);
-PrintCheckIfError(FindNumberByPosition(myArray, 3, 3), 3, 3);
+PrintCheckIfError(FindNumberByPosition(myArray, 2, 2), 2, 2);
+*/
+
+/*
+----------- Задача 3. ----------------------------------------------------
+
+Внутри класса Answer напишите методы CreateIncreasingMatrix, PrintArray, 
+PrintListAvr и FindAverageInColumns.
+
+Метод CreateIncreasingMatrix должен создавать матрицу заданной размерности, 
+с каждым новым элементом увеличивающимся на определенное число. 
+Метод принимает на вход три числа (n - количество строк матрицы, 
+m - количество столбцов матрицы, k - число, на которое увеличивается 
+каждый новый элемент) и возвращает матрицу, удовлетворяющую этим условиям.
+
+Метод PrintArray должен выводить на экран сгенерированную методом 
+CreateIncreasingMatrix матрицу.
+
+Метод FindAverageInColumns принимает целочисленную матрицу 
+типа int[,] и возвращает одномерный массив типа double. 
+Этот метод вычисляет среднее значение чисел в каждом столбце матрицы 
+и сохраняет результаты в виде списка.
+
+Метод PrintListAvr принимает одномерный массив, 
+возвращенный методом FindAverageInColumns и выводит 
+этот список на экран в формате 
+"The averages in columns are: x.x0 x.x0 x.x0 ...", 
+где x.x0 - это значения средних значений столбцов, 
+округленные до двух знаков после запятой 
+(в дробной части ВСЕГДА должно быть 2 числа через точку, см. формат вывода), 
+разделенные знаком табуляции.
+
+Пример
+
+
+n = 3;
+m = 4;
+k = 2;
+
+int[,] result = CreateIncreasingMatrix(n, m, k);
+PrintArray(result);
+PrintListAvr(FindAverageInColumns(result));
+
+1   3   5   7   
+9   11  13  15  
+17  19  21  23  
+The averages in columns are:
+9.00    11.00   13.00   15.00
+*/
+
+int InputInteger(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] matrix)
+{
+    // Введите свое решение ниже
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int[,] CreateIncreasingMatrix(int n, int m, int k)
+{
+    // Введите свое решение ниже
+    int[,] array = new int[n, m];
+    int temp = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            array[i, j] = temp;
+            temp += k;
+        }
+    }
+    return array;
+
+}
+
+// Метод PrintListAvr принимает одномерный массив, 
+// возвращенный методом FindAverageInColumns и выводит 
+// этот список на экран в формате 
+// "The averages in columns are: x.x0 x.x0 x.x0 ...", 
+// где x.x0 - это значения средних значений столбцов, 
+// округленные до двух знаков после запятой 
+// (в дробной части ВСЕГДА должно быть 2 числа через точку, 
+// см. формат вывода), 
+// разделенные знаком табуляции.
+
+void PrintListAvr(double[] list)
+{
+    // Введите свое решение ниже
+    System.Console.WriteLine("The averages in columns are:");
+    for (int i = 0; i < list.Length; i++)
+    {
+        System.Console.Write($"{(list[i]):f2}\t");
+    }
+
+}
+
+// Метод FindAverageInColumns принимает целочисленную матрицу 
+// типа int[,] и возвращает одномерный массив типа double. 
+// Этот метод вычисляет среднее значение чисел в каждом столбце матрицы 
+// и сохраняет результаты в виде списка.
+
+double[] FindAverageInColumns(int[,] matrix)
+{
+    // Введите свое решение ниже
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    double[] array = new double[columns];
+    double temp = 0;
+    for (int j = 0; j < columns; j++)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            temp = temp + matrix[i, j];
+        }
+        array[j] = temp / (rows);
+        temp = 0;
+    }
+    return array;
+}
+
+int rows = InputInteger("Введите количество строк: ");
+int columns = InputInteger("Введите количество столбцов: ");
+int number = InputInteger("Введите число, на которое увеличивается каждый новый элемент: ");
+
+int[,] result = CreateIncreasingMatrix(rows, columns, number);
+PrintArray(result);
+PrintListAvr(FindAverageInColumns(result));
